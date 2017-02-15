f00e03189d59af36e39eae3b2965c2c0035b2c36
Erica Wright and Matt Kelley <matt@>
2017-02-15 10:32:35 -0800

    add template to display view on webpage

--- app/app.php
+++ app/app.php
@@ -1,16 +1,10 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/RockPaperScissors.php";
+    require_once __DIR__."/../src/FindReplace.php";
 
     use Symfony\Component\Debug\Debug;
     Debug::enable();
-
-    session_start();
-
-    // if (empty($_SESSION['game_rounds'])) {
-    //     $_SESSION['game_rounds'] = array();
-    // }
 
     $app = new Silex\Application();
 
@@ -22,14 +16,18 @@
     ));
 
     $app->get("/", function() use ($app) {
-        return $app['twig']->render('form.html.twig',array('results'=>array()));
+        return $app['twig']->render('form.html.twig',array('result'=>array()));
     });
 
     $app->post("/", function() use ($app) {
+        $new_phrase = new FindReplace();
+        $sentence = $_POST['sentence'];
+        $word_to_search = $_POST['word_to_search'];
+        $replace_with = $_POST['replace_with'];
 
+        $result = $new_phrase->regexReplace($sentence, $word_to_search, $replace_with);
 
-
-        return $app['twig']->render('form.html.twig',array());
+        return $app['twig']->render('form.html.twig', array('result' => $result));
     });
 
     return $app;
